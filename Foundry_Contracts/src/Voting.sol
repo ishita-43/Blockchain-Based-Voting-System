fa01c4be8ce81ab9b45b6e49bc9a341f7e0c3917
Shiv Rai <sshaktirai@gmail.com>
2024-10-05 19:48:46 +0530

    Foundry contracts

---
+++ Foundry_Contracts/src/Voting.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract Voting{
+
+struct Voter{
+  bool voted;
+  uint vote;
+  bool authorized;
+ }
+
+struct Candidate{
+ uint id;
+ string name;
+ uint voteCount;
+}
+
+address public owner;
+string public electionName;
+
+mapping(address => Voter) public voters;
+Candidate[] public candidates;
+uint public totalVotes;
+
+
+constructor(string memory _name) {
+  owner = msg.sender;
+  electionName = _name;
+}
+
+error Voting__NotOwner();
+error Voting__AlreadyVoted();
+error Voting__NotAuthorized();
+error Voting__IncorrectVoteIndex();
+
+
+modifier ownerOnly(){
+ require(msg.sender == owner, Voting__NotOwner());
+ _;
+}
+
+function addCandidate(string memory _name) public ownerOnly{
+  candidates.push(Candidate(candidates.length, _name, 0));
+}
+
+function authorize(address _person) public ownerOnly {
+    voters[_person].authorized = true;
+}
+
+function vote(uint _voteIndex) public {
+  require(!voters[msg.sender].voted, Voting__AlreadyVoted());
+  require(voters[msg.sender].authorized, Voting__NotAuthorized());
+  require(_voteIndex<candidates.length, Voting__IncorrectVoteIndex());
+  voters[msg.sender].vote = _voteIndex;
+  voters[msg.sender].voted = true;
+  candidates[_voteIndex].voteCount += 1;
+  totalVotes +=1;
+}
+
+function getCandidates() public view returns (Candidate[] memory) {
+  return candidates;
+}
+
+function getTotalVotes() public view returns (uint) {
+  return totalVotes;
+}
+
+}
